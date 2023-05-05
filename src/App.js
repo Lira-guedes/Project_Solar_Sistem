5c4fd59aa9fe5afe52789dc84bc8d1f6c06fb477
Lira Guedes <liracrg@gmail.com>
2023-05-05 14:07:41 -0300

    Corrigindo erros

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Missions from './data/missions';
+import Missions from './components/Missions';
+import './App.css';
 
 class App extends React.Component {
   render() {
--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -5,9 +5,11 @@ class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
-        <p data-testid="planet-name">{ planetName }</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      <div id="imgp">
+        <div data-testid="planet-card">
+          <p data-testid="planet-name">{ planetName }</p>
+          <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        </div>
       </div>
     );
   }
